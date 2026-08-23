4d7605dbe1ed089f3cc76c9cef3677f5f15047b5
Realzzy <82355841+then77@users.noreply.github.com>
2026-08-23 17:40:16 +0700

    feat(bot): Add bot client base

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+if (!Bun.env.DISCORD_TOKEN) {
+    throw new Error("DISCORD_TOKEN is not set yet.");
+}
+await client.login(Bun.env.DISCORD_TOKEN);
