66dc9c85546b69425ee3aa5d1a27e9bb1e2d2771
Hexaa <me@hexaa.sh>
2026-09-01 18:48:21 +0700

    feat(web): added svelte, tailwindcss, and cloudflare support

--- apps/admin-web/astro.config.mjs
+++ apps/admin-web/astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: cloudflare(),
+  integrations: [react(), svelte()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
